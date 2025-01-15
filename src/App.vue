945fc954ee1eeadc40fb623508d0a2301274ecd1
Kontorsk <ko.il5@mail.ru>
2025-01-15 18:28:49 +0500

    profect create

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <TheNavbar />
+  <div class="container with-nav">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import TheNavbar from "./components/TheNavbar";
+
+export default {
+  components: {
+    TheNavbar,
+  },
+};
+</script>
